c89f1118c139172c525f5cccae15047a2702996b
Ashley Stith <71471256+stithac@users.noreply.github.com>
2021-04-01 23:58:17 -0400

    Final update to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,7 +12,7 @@ function Navbar() {
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
           <button className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></button>
-
+          <button className="nav-item nav-link" href="#">Card View</button>
         </div>
       </div>
     </nav>
